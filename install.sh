f3aca230fed1301981ad4ff7dee93c80bf19144b
Pekka Enberg <penberg@turso.tech>
2023-07-20 10:23:08 +0300

    Revert "Fix "not found" error on fresh install"
    This reverts commit ce3d1ec4c32da94d83eb2ea725de528a82ba8fa0. It broke
    installs.

--- install.sh
+++ install.sh
@@ -74,14 +74,16 @@ detect_profile() {
   local SHELLTYPE
   SHELLTYPE="$(basename "/$SHELL")"
 
-  if [ -n "$BASH" ]; then
+  if [ "$SHELLTYPE" = "bash" ]; then
     if [ -f "$HOME/.bashrc" ]; then
       DETECTED_PROFILE="$HOME/.bashrc"
     elif [ -f "$HOME/.bash_profile" ]; then
       DETECTED_PROFILE="$HOME/.bash_profile"
     fi
-  elif [ -n "$ZSH_NAME" ]; then
+  elif [ "$SHELLTYPE" = "zsh" ]; then
     DETECTED_PROFILE="$HOME/.zshrc"
+  elif [ "$SHELLTYPE" = "fish" ]; then
+    DETECTED_PROFILE="$HOME/.config/fish/conf.d/turso.fish"
   fi
 
   if [ -z "$DETECTED_PROFILE" ]; then
@@ -148,4 +150,4 @@ printf "\nTurso CLI installed!\n\n"
 printf "If you are a new user, you can sign up with ${bright_blue}turso auth signup${reset}.\n\n"
 printf "If you already have an account, please login with ${bright_blue}turso auth login${reset}.\n\n"
 
-$INSTALL_DIRECTORY/turso auth signup
+turso auth signup
