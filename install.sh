4f371c31f12c2951bbe2e56eba66a669fcbf410e
Avinash Sajjanshetty <opensource@avi.im>
2023-12-14 19:16:54 +0530

    Add skip signup flag

--- install.sh
+++ install.sh
@@ -155,4 +155,7 @@ printf "\nTurso CLI installed!\n\n"
 printf "If you are a new user, you can sign up with ${bright_blue}turso auth signup${reset}.\n\n"
 printf "If you already have an account, please login with ${bright_blue}turso auth login${reset}.\n\n"
 
-$INSTALL_DIRECTORY/turso auth signup
+# if SKIP_SIGNUP is set, then skip the sign up command
+if [ -z "$SKIP_SIGNUP" ]; then
+  $INSTALL_DIRECTORY/turso auth signup
+fi
